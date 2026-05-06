36bf26a8f42aba352bd6f8015b6c9ddea718b29f
Dataram57 <contact@dataram57.com>
2026-05-06 22:18:27 +0200

    move to top example

--- src/app/features/tarot/reflection-board/component.ts
+++ src/app/features/tarot/reflection-board/component.ts
@@ -3,50 +3,52 @@ import { CommonModule } from '@angular/common';
 
 // Definitions
 export interface Card {
-    id: string;
-    name: string;
-    src: string;
+    id : string;
+    name : string;
+    src : string;
 }
 
 export interface Deck {
-    name: string;
-    cards: Card[];
+    name : string;
+    cards : Card[];
 }
 
 interface CardInstance {
-    id: string;
-    name: string;
-    src: string;
-    x: number;
-    y: number;
-    rotation: number;
+    id : string;
+    name : string;
+    src : string;
+    x : number;
+    y : number;
+    rotation : number;
+    shiftRotation : number;
 }
 
 @Component({
-    selector: 'reflection-board',
-    standalone: true,
-    imports: [CommonModule],
-    templateUrl: './component.html',
-    styleUrls: ['./component.css']
+    selector : 'reflection-board',
+    standalone : true,
+    imports : [CommonModule],
+    templateUrl : './component.html',
+    styleUrls : ['./component.css']
 })
 export class ReflectionBoard {
 
-    @Input() decks: Deck[] = [];
+    @Input() decks : Deck[] = [];
 
     cards = signal<CardInstance[]>([]);
 
     // =========================
     // Add card
-    addCard(card: Card) {
+    addCard(card : Card) {
         this.cards.update(cards => [
             ...cards,
             {
                 id: crypto.randomUUID(),
                 name: card.name,
                 src: card.src,
-                x: 80,
-                y: 80,
-                rotation: 0
+                x: 0,
+                y: 0,
+                rotation: 0,
+                shiftRotation: 0
             }
         ]);
     }
@@ -78,23 +80,24 @@ export class ReflectionBoard {
             dx = e.clientX - startX;
             dy = e.clientY - startY;
 
-            card.rotation = 0;
-            target.style.transform = this.GetTransform(card.x + dx, card.y + dy, card.rotation);
+            card.shiftRotation = 0;
+            target.style.transform = this.GetTransform(card.x + dx, card.y + dy, card.rotation + card.shiftRotation);
         };
 
         const up = (e: PointerEvent) => {
             target.releasePointerCapture(e.pointerId);
 
             //snap to grid
-            card.x = this.snap(card.x + dx, target.clientWidth / 2);
-            card.y = this.snap(card.y + dy, target.clientHeight / 2);
-            card.rotation = Math.random() * 4 - 2;
+            const isRotated = (card.rotation / 90) % 2 == 0;
+            card.x = this.snap(card.x + dx, (isRotated ? target.clientWidth : target.clientHeight) / 2);
+            card.y = this.snap(card.y + dy, (isRotated ? target.clientHeight : target.clientWidth) / 2);
+            card.shiftRotation = Math.random() * 4 - 2;
 
             //enable transition
             target.style.transition = 'transform 250ms cubic-bezier(0.22, 1, 0.36, 1)';
 
             // animate to snapped position
-            target.style.transform = this.GetTransform(card.x, card.y, card.rotation);
+            target.style.transform = this.GetTransform(card.x, card.y, card.rotation + card.shiftRotation);
 
             window.removeEventListener('pointermove', move);
             window.removeEventListener('pointerup', up);
@@ -110,20 +113,23 @@ export class ReflectionBoard {
         return `translate3d(${x}px, ${y}px, 0) rotate(${rotation}deg)`;
     }
 
-    // =========================
-    // Rotate
     rotate(card: CardInstance) {
         this.cards.update(cards =>
             cards.map(c =>
                 c.id === card.id
-                    ? { ...c, rotation: (c.rotation + 180) % 360 }
+                    ? { ...c, rotation: (c.rotation + 90) % 360 }
                     : c
             )
         );
     }
 
-    // =========================
-    // Remove
+    front(card: CardInstance) {
+        this.cards.update(cards => {
+            const filtered = cards.filter(c => c.id !== card.id);
+            return [...filtered, card]; // move to end (top)
+        });
+    }
+
     remove(card: CardInstance) {
         this.cards.update(cards => cards.filter(c => c.id !== card.id));
     }
